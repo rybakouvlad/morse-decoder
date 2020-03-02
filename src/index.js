ab9e84506d402b8482a5c1054ef0eba645c40319
rybakouvlad <rybakou.vladik@gmail.com>
2020-03-03 02:10:23 +0300

    feat: add morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    for (let i = 0; i <= expr.length; i = i + 10) {
+        let match = "";
+        let kuku = [];
+        if (expr.slice(i, i + 10).length == 10) {
+          match = expr.slice(i, i + 10);
+          if (expr.slice(i, i + 10) == "**********") {
+            kuku.push(" ");
+            morze.push(" ");
+          }
+    
+          for (let n = 0; n < 10; n++) {
+            if (match[n] + match[n + 1] == "10") {
+              kuku.push(".");
+              n++;
+            } else if (match[n] + match[n + 1] == "11") {
+              kuku.push("-");
+              n++;
+            }
+          }
+        }
+        morze.push(MORSE_TABLE[kuku.join("")]);
+      }
+      return morze.join("");// write your solution here
 }
 
 module.exports = {
